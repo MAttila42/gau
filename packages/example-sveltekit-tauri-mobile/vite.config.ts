8f6660b01489d9a03232e126fe52cd7ff8d506dd
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-09-06 18:12:13 +0200

    chore: mobile

--- packages/example-sveltekit-tauri-mobile/vite.config.ts
+++ packages/example-sveltekit-tauri-mobile/vite.config.ts
@@ -3,12 +3,22 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
+const host = process.env.TAURI_DEV_HOST
 const isTauri = !!process.env.TAURI_ENV_PLATFORM
 
 export default defineConfig({
   plugins: [sveltekit(), UnoCSS()],
+  clearScreen: false,
   server: {
     port: isTauri ? 4173 : 5173,
     strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1430,
+        }
+      : undefined,
   },
 })
